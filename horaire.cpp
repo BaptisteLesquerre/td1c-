54a4444d34400c139e1d0c26bb829a1b3c735e52
BaptisteLesquerre <116633910+BaptisteLesquerre@users.noreply.github.com>
2022-10-25 12:01:46 +0200

    Update horaire.cpp

--- horaire.cpp
+++ horaire.cpp
@@ -2,26 +2,26 @@
 #include "horaire.h"
 #include <ctime>
 
-Horraire::Horaire(int heure,int minute,int seconde):_heure(heure),_minute(minute),_seconde(seconde){
+horaire::Horaire::Horaire(int heure,int minute,int seconde):_heure(heure),_minute(minute),_seconde(seconde){
     bool status = isHoraire(heure,minute,seconde);
     assert(status && "Horaire is not valid");
 }
 
-bool Horaire::isHoraire(int heure,int minute,int seconde) {
+bool horaire::Horaire::isHoraire(int heure,int minute,int seconde) {
     if ((heure < 0 ) || (heure> 23)) return false;
     if ((minute<0) || (minute>59)) return false;
     if ((seconde<0) || (seconde>59)) return false;
 }
 
-int Horaire::heure() const{
+int horaire::Horaire::heure() const{
      return _heure;
 }
 
-int Horaire::minute() const{
+int horaire::Horaire::minute() const{
      return _minute;
 }
 
-int Date::seconde() const{
+int horaire::Horaire::seconde() const{
      return _seconde;
 }
 
