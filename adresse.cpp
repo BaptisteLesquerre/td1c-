77752b3e66d50dbf08bbe59594ba63ea58343811
BaptisteLesquerre <116633910+BaptisteLesquerre@users.noreply.github.com>
2022-10-26 21:33:38 +0200

    Update adresse.cpp

--- adresse.cpp
+++ adresse.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
 #include "adresse.h"
 
-Adresse::Adresse(std::string nbrue,std::string rue,std::string codePost,std::string ville,std::string pays):_nbrue(nbrue),_rue(rue),_codePost(codePost),_ville(ville),_pays(pays){
+adresse::Adresse::Adresse(std::string nbrue,std::string rue,std::string codePost,std::string ville,std::string pays):_nbrue(nbrue),_rue(rue),_codePost(codePost),_ville(ville),_pays(pays){
 }
 
-std::string Adresse::nbrue() const{
+std::string adresse::Adresse::nbrue() const{
     return _nbrue;
 }
 
-std::string Adresse::rue() const{
+std::string adresse::Adresse::rue() const{
     return _rue;
 }
 
-std::string Adresse::codePost() const{
+std::string adresse::Adresse::codePost() const{
     return _codePost;
 }
 
-std::string Adresse::ville() const{
+std::string adresse::Adresse::ville() const{
     return _ville;
 }
 
-std::string Adresse::pays() const{
+std::string adresse::Adresse::pays() const{
     return _pays;
 }
